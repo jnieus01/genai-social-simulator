e2843cdc6c8b4ad7b50129b2af540a88a4caa2d9
Jordan <88906408+jnieus01@users.noreply.github.com>
2025-06-09 15:52:55 -0400

    Add utils for analytics tracking, data export, logging, channel observability, and seeding conversations

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from utils.analytics_tracker import AnalyticsTracker
+from utils.logger import get_logger
+import utils.colors as colorize
+import utils.channel_observer as channel_observer
+import utils.seed_conversation as seed_conversation
+
+__all__ = [
+    "AnalyticsTracker",
+    "get_logger",
+    "colorize",
+    "channel_observer",
+    "seed_conversation"
+]
---
+++ utils/analytics_tracker.py
@@ -0,0 +1,26 @@
+import time
+
+class AnalyticsTracker:
+    def __init__(self, redis_client, bot_name):
+        self.client = redis_client
+        self.bot_name = bot_name
+
+    def track_generation(self, channel):
+        key = f"analytics:{self.bot_name}:{channel}:generations"
+        self.client.incr(key)
+
+    def track_turn(self, channel):
+        key = f"analytics:{self.bot_name}:{channel}:turns"
+        self.client.incr(key)
+
+    def track_message_received(self, channel):
+        key = f"analytics:{self.bot_name}:{channel}:received"
+        self.client.incr(key)
+
+    def track_message_ignored(self, channel):
+        key = f"analytics:{self.bot_name}:{channel}:ignored"
+        self.client.incr(key)
+
+    def track_processing_time(self, channel, duration):
+        key = f"analytics:{self.bot_name}:{channel}:processing_time"
+        self.client.rpush(key, duration)
